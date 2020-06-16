fca171c4609a8f4880bf75167f4c836fb2458e24
ruz1k <fastfodd4@yandex.ru>
2020-06-16 14:26:32 +0300

    finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
 import schedule
-import datetime
 import time
 import random
 import mysql.connector
@@ -12,18 +11,13 @@ id = 455257905
 #connection with mysql
 mydb = mysql.connector.connect(
     host="127.0.0.1",
-    user="root",
-    password="root",
+    user="*****",
+    password="****",
     port="3306",
-    database="telebot"
+    database="test"
 )
 
 cursor = mydb.cursor()
-# create database in mysql
-# cursor.execute("CREATE DATEBASE telebot")
-# drop and create table commands in mysql
-# cursor.execute("DROP TABLE user_date")
-# cursor.execute("CREATE TABLE user_date (date_otpravki TIME, date_otveta TIME)")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -34,35 +28,30 @@ def start_message(message):
 def schedule_checker():
     while True:
         schedule.run_pending()
-        time.sleep(random.randint(10,100))
+        time.sleep(random.randint(10,20))
 
 
 # this block asks question to the user
 def question_message():
     msg = bot.send_message(id, "Не спишь?")
-    ot_date = datetime.datetime.now().time()
-    x = ot_date.strftime("%H:%M:%S")
-    sql = "INSERT INTO user_date (date_otpravki) \
-                                      VALUES (%s)"
-    val = (x)
-    cursor.execute(sql, (val,))
+    sql = "INSERT INTO user_date SET request=NOW()"
+    cursor.execute(sql)
     mydb.commit()
     bot.register_next_step_handler(msg, answer_message)
 
 
-# in this block user gives answer for the question messag3
+# in this block user gives answer for the question message
 def answer_message(message):
     try:
-        date = datetime.datetime.now().time()
-        q = date.strftime("%H:%M:%S")
+        i = cursor.lastrowid
         #update mysql base
-        sql = "UPDATE user_date SET date_otveta = %s"
-        val = (q)
+        sql = "UPDATE user_date SET response = NOW() WHERE id = %s"
+        val = (i)
         cursor.execute(sql, (val,))
         mydb.commit()
         bot.send_message(id, "Хорошо!")
     except Exception as e:
-        bot.reply_to(message, 'oops')
+        bot.reply_to(message, 'Ошибка!')
 
 
 if __name__ == "__main__":
@@ -70,7 +59,7 @@ if __name__ == "__main__":
     i = 0
     for i in range(1):
         delay = random.randrange(1, 12)
-        schedule.every(delay).hours.do(question_message)
+        schedule.every(delay).seconds.do(question_message)
 
     Thread(target=schedule_checker).start()
 
