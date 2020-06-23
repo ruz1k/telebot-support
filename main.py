83be7512ba092ce1bb6f06bfa7742663d4fb1117
Arthur <46713689+ruz1k@users.noreply.github.com>
2020-06-23 11:33:07 +0300

    Last update, finish state

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
-import telebot
 import schedule
 import time
 import random
 import mysql.connector
 from threading import Thread
+import telebot
+from telebot import types
 
-bot = telebot.TeleBot('1194470771:AAGDMTviS3zdweu_xmJgfOwhB7bPlG1O7DU')
-id = 455257905
+bot = telebot.TeleBot('***')
 
 i = 0
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    user = message.from_user.first_name
-    bot.send_message(message.chat.id, "Hello, {}, I Support Bot".format(user))
-
+id = **
 
 def schedule_checker():
     while True:
         schedule.run_pending()
-        time.sleep(random.randint(5, 10))
+        time.sleep(1)
 
 
 # this block asks question to the user
 def question_message():
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="***",
-            password="***",
-            port="3306",
-            database="test"
-            )
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_net = types.KeyboardButton(text="/Net")
+    keyboard.add(button_net)
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="bet2u_db",
+    password="2V6e5R8g",
+    port="3306",
+    database="test"
+    )
     cursor = mydb.cursor()
-    msg = bot.send_message(id, "Hi are you sleeping?")
+    msg = bot.send_message(id, "Ti, ne spish?", reply_markup=keyboard)
     sql = "INSERT INTO maksBot SET request=NOW()"
     cursor.execute(sql)
     mydb.commit()
@@ -41,35 +40,31 @@ def question_message():
     bot.register_next_step_handler(msg, answer_message)
 
 # in this block user gives answer for the question message
+@bot.message_handler(commands=["Net"])
 def answer_message(message):
     try:
         global i
-        print(i)
         mydb = mysql.connector.connect(
             host="localhost",
-            user="***",
-            password="***",
+            user="bet2u_db",
+            password="2V6e5R8g",
             port="3306",
             database="test"
             )
         cursor = mydb.cursor()
         #update mysql base
-        sql = "UPDATE maksBot SET response = NOW() WHERE id = %s"
+        sql = "UPDATE `maksBot` SET `response`= CASE WHEN `response` = '0000-00-00 00:00:00' THEN NOW() ELSE `response` END WHERE `id` = %s"
         val = (i)
         cursor.execute(sql, (val,))
         mydb.commit()
-        bot.send_message(id, "Okay!")
+        bot.send_message(id, "Horosho!")
     except Exception as e:
-        bot.send_message(id, 'Error!')
+        bot.send_message(id, 'Error\n{}'.format(e))
 
 
 if __name__ == "__main__":
-    #in this block bot sends message
     j = 0
     for j in range(1):
-        delay = random.randrange(1, 8)
-        schedule.every(delay).hours.do(question_message)
-
+        schedule.every(6).to(10).hours.do(question_message)
     Thread(target=schedule_checker).start()
-
-bot.polling()
+    bot.polling(none_stop=True)
